7684890808509499f6fe40f6869b84e52526118e
Connor McCormack <mccormack.connor.m@gmail.com>
2020-04-03 15:50:28 -0500

    POST and DELETE working

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
-
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
 
@@ -17,28 +16,12 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
-  .then(articles => {
-    res.json(articles)
-  })
-  .catch(next)
-})
+app.use('/articles', articlesRouter)
 
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: `Article doesn't exist` }
-        })
-      }
-      res.json(article)
-    })
-    .catch(next)
-})
+app.get('/xss', (req, res) => {
+  res.cookie('secretToken', '1234567890');
+  res.sendFile(__dirname + '/xss-example.html');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
--- src/articles/articles-service.js
+++ src/articles/articles-service.js
@@ -27,3 +27,4 @@ const ArticlesService = {
 }
 
 module.exports = ArticlesService
+
